c097e75b0731da5e3a954152a0df4a8cb6974901
steam228 <steam228@gmail.com>
2024-05-02 14:33:34 +0100

    entregafinal

--- sketch.js
+++ sketch.js
@@ -1,11 +1,25 @@
 function setup() {
-  createCanvas(1280, 1024);
-  background(153);
-  stroke(0);
-  strokeWeight(2);
-  fill(237, 28, 36);
-  circle(30, 30, 40);
-  line(30, 30, 1280 / 2, 1024 / 2);
+  createCanvas(windowWidth, windowHeight);
+  background(255);
 }
 
-function draw() {}
+function draw() {
+  background(255);
+  stroke(0);
+  strokeWeight(3);
+
+  if (mouseX < width / 2) {
+    if (mouseY < height / 2) {
+      fill(255, 0, 0);
+    } else {
+      fill(0, 255, 0);
+    }
+  } else {
+    if (mouseY < height / 2) {
+      fill(0, 0, 255);
+    } else {
+      fill(255);
+    }
+  }
+  circle(width / 2, height / 2, height / 4);
+}
